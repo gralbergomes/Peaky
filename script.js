a47982c1b06321fa1a1bc3f07d96ab5e33b1dfc8
Rusty <gralbergs@gmail.com>
2025-03-14 19:46:13 -0300

    correção

--- script.js
+++ script.js
@@ -6,8 +6,3 @@ menu.addEventListener('click', () => {
     NavMenu.classList.toggle('ativo');
 })
 
-// Função para tocar a música automaticamente após a interação do menu
-window.onload = function() {
-    var audio = document.getElementById('background-audio');
-    audio.play(); // Força a reprodução do áudio
-};
