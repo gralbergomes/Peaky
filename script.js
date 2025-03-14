aedf84477dd35945fa8f065ac9494e99ac9f3425
Rusty <gralbergs@gmail.com>
2025-03-14 19:10:18 -0300

    testemusica

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
+
+// Função para tocar a música automaticamente após a interação do menu
+window.onload = function() {
+    var audio = document.getElementById('background-audio');
+    audio.play(); // Força a reprodução do áudio
+};
